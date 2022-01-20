e41de26fa0b63ed8c82a1974800736c8aa480f4e
Colby Patterson <cpatt@byui.edu>
2022-01-19 19:21:28 -0700

    Added roll method for Die class

--- dice/game/die.py
+++ dice/game/die.py
@@ -35,3 +35,10 @@ class Die:
         
 #         Args:
 #             self (Die): An instance of Die
+
+    def roll(self):
+        self.value = random.randint(1, 6)
+        if self.value == 1:
+            self.points = 100
+        elif self.value == 50:
+            self.points == 50
